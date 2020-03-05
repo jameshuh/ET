e7c52a1dc256f1b312c77e693c0bb49f7010dc20
DukeChiang <dukechiang@qq.com>
2020-03-05 16:50:37 +0800

    重新规划Server目录结构与客户端保持一致，利于理解

---
+++ Server/Assets/Hotfix/Helper/TypeHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace DCET
+{
+	public static class TypeHelper
+	{
+		public static void InitAllType()
+		{
+			Game.EventSystem.AddAssemblyType("DCET.Core");
+			Game.EventSystem.AddAssemblyType("DCET.Config");
+			Game.EventSystem.AddAssemblyType("DCET.CoroutineLock");
+			Game.EventSystem.AddAssemblyType("DCET.DB");
+			Game.EventSystem.AddAssemblyType("DCET.Message");
+			Game.EventSystem.AddAssemblyType("DCET.Numeric");
+			Game.EventSystem.AddAssemblyType("DCET.Pathfinding");			
+			Game.EventSystem.AddAssemblyType("DCET.BehaviorTree");
+			Game.EventSystem.AddAssemblyType("DCET.Runtime");
+			Game.EventSystem.AddAssemblyType("DCET.Hotfix");
+			Game.EventSystem.AddAssemblyType("DCET.Benchmark");
+		}
+	}
+}
